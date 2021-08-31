3a814b9d3da6befc67348a09728580c03cb2aee4
kuro11pow2 <kuro11pow2@gmail.com>
2021-08-31 23:06:05 +0900

    렌더링 결과 팝업 창 비활성화 기능 추가

--- recorder/recorder.py
+++ recorder/recorder.py
@@ -1,18 +1,32 @@
 import gym
 
 class Recorder:
-    def __init__(self, env):
+    def __init__(self, env, result_window=True):
         self.n_epi_set = set()
-        self.env = gym.wrappers.Monitor(env, "./gym-results", force=True, video_callable=self.save_or_not)
+        self.env = gym.wrappers.Monitor(env, "./gym-results", force=True, video_callable=self._save_or_not)
+        if not result_window:
+            self._disable_window_classic_control()
 
-    def update(self, n_epi_li):
-        if hasattr(n_epi_li, '__iter__') and all(map(lambda x : type(x) == type(int()), n_epi_li)):
-            self.n_epi_set.update(n_epi_li)
+    def update(self, n_epi_it):
+        if hasattr(n_epi_it, '__iter__') and all(map(lambda x : type(x) == type(int()), n_epi_it)):
+            self.n_epi_set.update(n_epi_it)
         else:
             raise Exception("Recorder에 잘못된 n_epi가 들어옴")
 
     def wrapped_env(self):
         return self.env
+
+    def _disable_window_classic_control(self):
+        from gym.envs.classic_control import rendering
+        org_constructor = rendering.Viewer.__init__
+
+        def constructor(self, *args, **kwargs):
+            org_constructor(self, *args, **kwargs)
+            self.window.set_visible(visible=False)
+
+        rendering.Viewer.__init__ = constructor
         
-    def save_or_not(self, n_epi):
+    def _save_or_not(self, n_epi):
         return n_epi in self.n_epi_set
+
+    
