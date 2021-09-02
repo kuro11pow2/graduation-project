952d4e06762a0f569471584a57110d54ef82d875
kuro11pow2 <kuro11pow2@gmail.com>
2021-09-02 23:08:43 +0900

    내부 변수에 underscore prefix 추가 (PEP 8)

--- recorder/recorder.py
+++ recorder/recorder.py
@@ -2,19 +2,19 @@ import gym
 
 class Recorder:
     def __init__(self, env, result_window=True):
-        self.n_epi_set = set()
-        self.env = gym.wrappers.Monitor(env, "./gym-results", force=True, video_callable=self._save_or_not)
+        self._n_epi_set = set()
+        self._env = gym.wrappers.Monitor(env, "./gym-results", force=True, video_callable=self._save_or_not)
         if not result_window:
             self._disable_window_classic_control()
 
     def update(self, n_epi_it):
         if hasattr(n_epi_it, '__iter__') and all(map(lambda x : type(x) == type(int()), n_epi_it)):
-            self.n_epi_set.update(n_epi_it)
+            self._n_epi_set.update(n_epi_it)
         else:
             raise Exception("Recorder에 잘못된 n_epi가 들어옴")
 
     def wrapped_env(self):
-        return self.env
+        return self._env
 
     def _disable_window_classic_control(self):
         from gym.envs.classic_control import rendering
@@ -27,6 +27,6 @@ class Recorder:
         rendering.Viewer.__init__ = constructor
         
     def _save_or_not(self, n_epi):
-        return n_epi in self.n_epi_set
+        return n_epi in self._n_epi_set
 
     
