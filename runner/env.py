02d82f234ea21dabfd6cea71a6fbdacb6577812c
kuro11pow2 <kuro11pow2@gmail.com>
2021-09-15 22:22:31 +0900

    env enum 클래스 생성

---
+++ runner/env.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+class Env(Enum):
+    CARTPOLE = 'CartPole-v1'
+    MOUNTAINCAR = 'MountainCar-v0'
+    PENDULUM = 'Pendulum-v0'
+    ACROBOT = 'Acrobot-v1'
+    LUNARLANDER = 'LunarLander-v2'
+    CARRACING = 'CarRacing-v0'
+    BIPEDALWALKER = 'BipedalWalker-v2'
