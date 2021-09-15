29c6bbc7c503591290e383b8c2c14d5641f64e38
kuro11pow2 <kuro11pow2@gmail.com>
2021-09-15 21:10:13 +0900

    ActorCritic 업로드

---
+++ runner/actorcritic_runner.py
@@ -0,0 +1,38 @@
+
+from runner import Runner, RunnerParams
+
+import torch
+
+from algorithms.actorcritic import ActorCritic
+from torch.distributions import Categorical
+
+class ActorCriticRunner(Runner):
+    def __init__(self, env_name, runner_params):
+        super(ActorCriticRunner, self).__init__(env_name, 'ActorCritic', runner_params)
+
+    def _episode_prepare(self):
+        n_state = self._env.observation_space.shape[0]
+        n_action = self._env.action_space.n
+        self._model = ActorCritic(n_state, n_action)
+        self._score = 0.0
+        self._print_interval = 20
+
+    def _episode_sim(self):
+        s = self._env.reset()
+        done = False
+        while not done:
+            for t in range(self._model.n_rollout):
+                prob = self._model.pi(torch.from_numpy(s).float())
+                m = Categorical(prob)
+                a = m.sample().item()
+                s_prime, r, done, info = self._env.step(a)
+
+                if self._train:
+                    self._model.put_data((s,a,r/self._reward_scale,s_prime,done))
+                
+                s = s_prime
+                self._score += r
+                if done:
+                    break                     
+            if self._train:
+                self._model.train_net()
---
+++ runner/algorithms/actorcritic.py
@@ -0,0 +1,67 @@
+import torch    # pytorch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class ActorCritic(nn.Module):
+    def __init__(self, n_state, n_action, n_node=256, learning_rate=0.0002, gamma=0.98, n_rollout=10):
+        super(ActorCritic, self).__init__()
+        self.n_state = n_state
+        self.n_action = n_action
+        self.n_node = n_node
+        self.learning_rate = learning_rate
+        self.gamma = gamma
+        self.n_rollout = n_rollout
+        self.data = []
+
+        self.fc1 = nn.Linear(self.n_state,self.n_node) 
+        self.fc_pi = nn.Linear(self.n_node,self.n_action) 
+        self.fc_v = nn.Linear(self.n_node,1)
+        self.optimizer = optim.Adam(self.parameters(), lr=self.learning_rate)
+        
+    def pi(self, x, softmax_dim = 0):
+        x = F.relu(self.fc1(x))
+        x = self.fc_pi(x)
+        prob = F.softmax(x, dim=softmax_dim)
+        return prob
+    
+    def v(self, x):
+        x = F.relu(self.fc1(x))
+        v = self.fc_v(x)
+        return v
+    
+    def put_data(self, transition):
+        self.data.append(transition)
+        
+    def make_batch(self):
+        s_lst, a_lst, r_lst, s_prime_lst, done_lst = [], [], [], [], []
+        for transition in self.data:
+            s,a,r,s_prime,done = transition
+            s_lst.append(s)
+            a_lst.append([a])
+            r_lst.append([r])
+            s_prime_lst.append(s_prime)
+            done_mask = 0.0 if done else 1.0
+            done_lst.append([done_mask])
+        
+        tmp = torch.tensor(s_lst, dtype=torch.float), \
+                            torch.tensor(a_lst), \
+                            torch.tensor(r_lst, dtype=torch.float), \
+                            torch.tensor(s_prime_lst, dtype=torch.float), \
+                            torch.tensor(done_lst, dtype=torch.float)
+        s_batch, a_batch, r_batch, s_prime_batch, done_batch = tmp
+        self.data = []
+        return s_batch, a_batch, r_batch, s_prime_batch, done_batch
+  
+    def train_net(self):
+        s, a, r, s_prime, done = self.make_batch()
+        td_target = r + self.gamma * self.v(s_prime) * done
+        delta = td_target - self.v(s)
+        
+        pi = self.pi(s, softmax_dim=1)
+        pi_a = pi.gather(1,a)
+        loss = -torch.log(pi_a) * delta.detach() + F.smooth_l1_loss(self.v(s), td_target.detach())
+
+        self.optimizer.zero_grad()
+        loss.mean().backward()
+        self.optimizer.step()
