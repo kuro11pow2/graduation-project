ce34edfaf4fe1a1b6570580f26b7c459458c109d
kuro11pow2 <kuro11pow2@gmail.com>
2021-09-20 01:28:21 +0900

    record_baseline 제거

--- src/runner.py
+++ src/runner.py
@@ -11,7 +11,7 @@ class RunnerParams:
     def __init__(self, *, save_net=False, name_postfix=None, load_net=False, load_name=None, 
                     train=True,  max_episode=10000, interval=20, 
                     max_video=3, video_record_interval=0,
-                    target_score=0, record_baseline=100, 
+                    target_score=0, 
                     reward_scale=1.0, step_wrapper=lambda x: x):
         self.save_net = save_net
         self.name_postfix = name_postfix
@@ -22,16 +22,23 @@ class RunnerParams:
         self.interval = interval
         self.max_video = max_video
         self.video_record_interval = video_record_interval
-        self.record_baseline = record_baseline
         self.target_score = target_score
         self.reward_scale = reward_scale
         self.step_wrapper = step_wrapper
+    
+    def __str__(self):
+        s = ''
+        s += f'train={self.train}-'
+        s += f'intvl={self.interval}-'
+        s += f'rwdscl={self.reward_scale}'
+        return s
 
 class Runner(metaclass=ABCMeta):
     def __init__(self, env_name, algo_name, algo_params, runner_params):
         self._env_name = env_name
         self._algo_name = algo_name
         self._algo_params = algo_params
+        self._runner_params = runner_params
         self._save_net = runner_params.save_net
         self._name_postfix = runner_params.name_postfix
         self._load_net = runner_params.load_net
@@ -41,7 +48,6 @@ class Runner(metaclass=ABCMeta):
         self._interval = runner_params.interval
         self._max_video = runner_params.max_video
         self._video_record_interval = runner_params.video_record_interval
-        self._record_baseline = runner_params.record_baseline
         self._target_score = runner_params.target_score
         self._reward_scale = runner_params.reward_scale
         self._step_wrapper = runner_params.step_wrapper
@@ -60,6 +66,7 @@ class Runner(metaclass=ABCMeta):
             raise Exception('discrete space만 지원됨.')
         name = f'runs/{self._algo_name}'
         name += f'-{self._env_name}'
+        name += f'-{str(self._runner_params)}'
         if self._name_postfix:
             name += f'-{self._name_postfix}'
         name += f'-{(str(int(time.time())))}'
@@ -82,25 +89,28 @@ class Runner(metaclass=ABCMeta):
             self._load()
 
         print('시뮬레이션 시작')
-        done = False
         for n_epi in range(self._max_episode):
-            self._episode_sim(n_epi)
-            done = self._is_done()
-
-            video_record = done
-            video_record = video_record or (self._video_record_interval and (n_epi % self._video_record_interval == 0))
-            avg_score = self._score / (n_epi % self._interval if n_epi % self._interval else self._interval)
-            video_record = video_record or avg_score >= self._record_baseline
-
-            if video_record:
-                self._record_video(n_epi, avg_score)
-
-            if n_epi % self._interval == 0:
-                self._print_log(n_epi)
+            reset_score = n_epi % self._interval == 0
+            if reset_score:
                 self._score = 0.0
 
-            if done:
-                print(f'종료 조건 만족함. 최종 {self._interval}번 평균 점수 {avg_score}')
+            record_video = self._video_record_interval and n_epi % self._video_record_interval == 0
+            if record_video:
+                self._recorder.record_start()
+                self._episode_sim(n_epi)
+                avg_score = self._score / (n_epi % self._interval + 1)
+                print(f'{n_epi=}, {avg_score=} 비디오 저장')
+                self._recorder.record_end()
+            else:
+                self._episode_sim(n_epi)
+
+            avg_score = self._score / (n_epi % self._interval + 1)
+            print_log = n_epi % self._interval == 0
+            if print_log:
+                self._print_log(n_epi)
+
+            if self._is_done(n_epi):
+                print(f'종료 조건 만족. 최종 {self._interval}번 평균 점수 {avg_score}')
                 self._end_score = avg_score
                 break
 
@@ -109,26 +119,17 @@ class Runner(metaclass=ABCMeta):
         if self._save_net:
             print('네트워크 저장')
             self._save()
-
-    def _record_video(self, n_epi, avg_score):
-        print(f'{n_epi=}, {avg_score=} 비디오 저장')
-        # name = f'{self._algo_name}'
-        # name += f'-{self._env_name}'
-        # name += f'-scr={avg_score}'
-        # name += f'-epi={n_epi}'
-        # if self._name_postfix:
-        #     name += f'-{self._name_postfix}'
-        # name += f'-{(str(int(time.time())))}'
-        self._recorder.add_epi([n_epi])
         
     def _print_log(self, n_epi):
-        print(f"에피소드: {n_epi}, 평균 점수: {self._score/self._interval:.1f}")
-        self._writer.add_scalar("score/train", self._score/self._interval, n_epi)
+        avg_score = self._score / (n_epi % self._interval + 1)
+        print(f"에피소드: {n_epi}, 평균 점수: {avg_score:.1f}")
+        self._writer.add_scalar("score/train", avg_score, n_epi)
     
-    def _is_done(self):
-        if (self._target_score <= self._score / self._interval):
+    def _is_done(self, n_epi):
+        avg_score = self._score / (n_epi % self._interval + 1)
+        if (self._target_score <= avg_score):
             return True
-        elif (len(self._recorder.recorded_epi()) >= self._max_video):
+        elif self._recorder.n_recorded >= self._max_video:
             return True
         else:
             return False
@@ -141,6 +142,7 @@ class Runner(metaclass=ABCMeta):
             name += f'{self._algo_name}'
             name += f'-{self._env_name}'
             name += f'-{self._end_score}'
+            name += f'-{str(self._runner_params)}'
             if self._name_postfix:
                 name += f'-{self._name_postfix}'
             name += f'-{(str(int(time.time())))}.pt'
